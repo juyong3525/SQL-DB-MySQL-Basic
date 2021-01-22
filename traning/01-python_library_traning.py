77cb89ac8e84bed1fe998f051e0122f42a00859a
Juyong Hong <juyong3525@gmail.com>
2021-01-22 11:56:59 +0900

    Added python library training

---
+++ traning/01-python_library_traning.py
@@ -0,0 +1,20 @@
+'''
+# 예제: 지수승 구하기
+print(2 ** 5)
+'''
+
+'''
+# 임의의 숫자와 지수를 입력으로 받아서, 지수승 구하기
+def exponential(digit, exponent):
+    value = digit ** exponent
+    return value
+print(exponential(3, 3))
+'''
+
+'''
+# math 라이브러리 사용
+import math as m
+num = m.pow(3, 3)
+print(num)
+print(m.factorial(5))
+'''
